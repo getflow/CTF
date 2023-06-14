62293b6e4d8a1014eb09764f1a5c719ac5143bd1
kartaris <rust768@gmail.com>
2023-06-14 17:01:08 +0300

    still draft

--- AtomSkills2023/fastapi-reverse/main.py
+++ AtomSkills2023/fastapi-reverse/main.py
@@ -1,31 +1,48 @@
+import uuid
+
 from fastapi import FastAPI
 from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.post("/commit")
-async def commit():
+@app.get("/{project_id}/commit")
+async def commit_list(project_id: uuid.UUID):
     pass
 
 
-@app.get("/pull")
-async def pull(name: str):
+@app.post("/{project_id}/commit")
+async def create_commit(project_id: uuid.UUID):
     pass
 
 
-@app.post("/push")
-async def push():
+@app.get("/{project_id}/commit/{commit_id}")
+async def get_commit(commit_id: uuid.UUID):
     pass
 
 
-@app.get("/fetch")
-async def fetch():
+@app.get("/{project_id}/mr")
+async def mr_list(project_id: uuid.UUID):
     pass
 
 
-@app.get("/")
-async def view():
+@app.post("/{project_id}/mr/{mr_id}")
+async def mr(project_id: uuid.UUID, mr_id: uuid.UUID):
+    pass
+
+
+@app.post("/{project_id}/mr")
+async def create_mr(project_id: uuid.UUID):
+    pass
+
+
+@app.get("/{project_id}/download")
+async def download(project_id: uuid.UUID):
+    pass
+
+
+@app.get("/{project_id}")
+async def view(project_id: uuid.UUID):
     return HTMLResponse("""<html>
     <head>
     </head>
